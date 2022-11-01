21ee84ba86d4af2aecc2e1e81b370f4ca6f91d17
Jefferson Luis <jeflsantos@hotmail.com>
2022-11-01 13:32:43 -0300

    kafka - Varios produtores e consumidores

--- src/main/java/br/com/alura/ecommerce/EmailService.java
+++ src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -8,30 +8,30 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-public class FraudDetectorService {
+public class EmailService {
 
 	public static void main(String[] args) {
 
 		var consumer = new KafkaConsumer<String, String>(properties());
-		consumer.subscribe(Collections.singletonList("ECOMMECE_NEW_ORDER"));
+		consumer.subscribe(Collections.singletonList("ECOMMECE_SEND_EMAIL"));
 		while (true) {
 			var records = consumer.poll(Duration.ofMillis(100));
 			if (!records.isEmpty()) {
 				System.out.println("Encontrei " + records.count() + " registros.");
 				for (var record : records) {
 					System.out.println("----------------------------");
-					System.out.println("Processing new order, checkinf for fraud");
+					System.out.println("Send email");
 					System.out.println(record.key());
 					System.out.println(record.value());
 					System.out.println(record.partition());
 					System.out.println(record.offset());
 					try {
-						Thread.sleep(5000);
+						Thread.sleep(1000);
 					} catch (InterruptedException e) {
 						// ignoring
 						e.printStackTrace();
 					}
-					System.out.println("Order processed");
+					System.out.println("Email sent");
 				}
 			}
 			
@@ -44,7 +44,7 @@ public class FraudDetectorService {
 		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
 		return properties;
 	}
 
